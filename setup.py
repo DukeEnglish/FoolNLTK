d4b3ff60e2ed2bf639eea34705317f1da4aee42f
lijunyi04 <lijunyi04@meituan.com>
2018-12-11 20:09:37 +0800

    change

--- setup.py
+++ setup.py
@@ -1,18 +1,10 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
-
+# 打包的时候，除了打包各种各样的文件之外，就是模型。之后就可以直接使用了
+#
 
 
 from distutils.core import setup
 
-setup(name = 'foolnltk',
-    version = '0.1.5',
-    description = 'Fool Nature Language toolkit ',
-    author = 'wu.zheng',
-    author_email = 'rocky.zheng314@gmail.com',
-    url = 'https://github.com/rockyzhengwu/FoolNLTK',
-    install_requires=['tensorflow>=1.3.0', 'numpy>=1.12.1'],
-    packages = ['fool',],
-    package_dir = {"fool": "fool"},
-    data_files = [("fool", ["data/map.zip", "data/pos.pb", "data/seg.pb", "data/ner.pb"])]
+setup(
     )
